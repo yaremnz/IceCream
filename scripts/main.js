f5f712806bc04e96b1c33a1c3d3515107bbefa51
Yareli Cerda <yarelic.1002@gmail.com>
2024-05-02 15:07:39 -0400

    js css edits

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,59 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+    
+    function updateDisplay(){
+        if(cupElement.checked){
+            toppings.classList.remove("hidden")
+        }else{
+            toppings.classList.add("hidden")
+        }
+    }
+    coneElement.onclick = updateDisplay;
+    cupElement.onclick = updateDisplay;
 
     submitOrder.addEventListener("click", ()=>{
+        
+        //GET AND CONVERT ELEMENT VALUES TO LOCAL JS VARIABLES
+        const scoops = Number(scoopsElement.value) 
+        const cone = coneElement.checked;  // NOTE CHECKED NOT VALUE
+        const cup = cupElement.checked; //EVEEN THOUGH ITS A RADIO
 
-        totalElement.innerText = 0
+        const sprinkes = sprinklesElement.ckecked;
+        const whipped = sprinklesElement.ckecked;
+        const fudge = hotFudgeElement.ckecked;
+        const cherry = cherryElement.ckecked
+        
+        // LOVAL JAVASCRIPT OUTPUT VARIABLES
+        let basePrice = 0;
+        let tax = 0; 
+        let total = 0;
+
+        //START LOGIC
+        basePrice = 2.25 ;
+        if(scoops > 1){
+            basePrice += 1.25 * (scoop - 1);
+        }
+        if(cup){
+            if(sprinkes){
+                basePrice += 0.50
+            }
+            if(whipped){
+                basePrice += 0.25
+            }
+            if(fudge){
+                basePrice += 1.25
+            }
+            if(cherry){
+                basePrice += 0.25
+            }
+        }
+        tax = basePrice * 0.08
+        total= basePrice + tax
+
+        //UPDATE DISPLAY
+        basePriceElement.innerText = "$" + basePrice.toFixed(2);
+        taxElement.innerText = "$" + tax.toFixed(2);
+        totalElement.innerText = "$" + total.toFixed(2);
+
 
     });//end click
 
